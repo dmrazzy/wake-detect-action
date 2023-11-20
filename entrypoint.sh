bf3e220f20424fd74cd687c46b50d774c87201fa
jaczkal <honza@pm.me>
2023-11-20 16:04:19 +0100

    🐛 Do not set envs to empty string

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,24 +7,23 @@
 
 WORKING_DIRECTORY="${1}"
 EXPORT_SARIF="${2}"
-export WAKE_CONFIG="${3}"
-export WAKE_COMPILE_ALLOW_PATHS="${4}" # Example: "[/tmp:/tmp]"
-export WAKE_COMPILE_EVM_VERSION="${5}" # Example: "prague"
-export WAKE_COMPILE_IGNORE_PATHS="${6}" # Example: "[/tmp:/tmp]"
-export WAKE_COMPILE_INCLUDE_PATHS="${7}" # Example: "[/tmp:/tmp]"
-export WAKE_COMPILE_OPTIMIZER_ENABLED="${8}" # Example: "[false]"
-export WAKE_COMPILE_OPTIMIZER_RUNS="${9}" # Example: "[0]"
-export WAKE_COMPILE_REMAPPINGS="${10}" # Example: "[]"
-export WAKE_COMPILE_TARGET_VERSION="${11}" # Example: "[0.8.9]"
-export WAKE_COMPILE_VIA_IR="${12}" # Example: "[false]"
-export WAKE_DETECT_MIN_IMPACT="${13}" # Example: "high"
-export WAKE_DETECT_MIN_CONFIDENCE="${14}" # Example: "high"
-export WAKE_DETECT_PATHS="${15}" # Example: "$1"
-export WAKE_DETECT_ONLY="${16}" # Example: "[bruh1 bruh2]"
-export WAKE_DETECT_EXCLUDE="${17}" # Example: "[bruh1 bruh2]"
-export WAKE_DETECT_IGNORE_PATHS="${18}" # Example: "[/tmp:/tmp]"
-export WAKE_DETECT_EXCLUDE_PATHS="${19}" # Example: "[/tmp:/tmp]"
-
+if [ -n "${3}" ]; then export WAKE_CONFIG="${3}"; fi
+if [ -n "${4}" ]; then export WAKE_COMPILE_ALLOW_PATHS="${4}"; fi
+if [ -n "${5}" ]; then export WAKE_COMPILE_EVM_VERSION="${5}"; fi
+if [ -n "${6}" ]; then export WAKE_COMPILE_IGNORE_PATHS="${6}"; fi
+if [ -n "${7}" ]; then export WAKE_COMPILE_INCLUDE_PATHS="${7}"; fi
+if [ -n "${8}" ]; then export WAKE_COMPILE_OPTIMIZER_ENABLED="${8}"; fi
+if [ -n "${9}" ]; then export WAKE_COMPILE_OPTIMIZER_RUNS="${9}"; fi
+if [ -n "${10}" ]; then export WAKE_COMPILE_REMAPPINGS="${10}"; fi
+if [ -n "${11}" ]; then export WAKE_COMPILE_TARGET_VERSION="${11}"; fi
+if [ -n "${12}" ]; then export WAKE_COMPILE_VIA_IR="${12}"; fi
+if [ -n "${13}" ]; then export WAKE_DETECT_MIN_IMPACT="${13}"; fi
+if [ -n "${14}" ]; then export WAKE_DETECT_MIN_CONFIDENCE="${14}"; fi
+if [ -n "${15}" ]; then export WAKE_DETECT_PATHS="${15}"; fi
+if [ -n "${16}" ]; then export WAKE_DETECT_ONLY="${16}"; fi
+if [ -n "${17}" ]; then export WAKE_DETECT_EXCLUDE="${17}"; fi
+if [ -n "${18}" ]; then export WAKE_DETECT_IGNORE_PATHS="${18}"; fi
+if [ -n "${19}" ]; then export WAKE_DETECT_EXCLUDE_PATHS="${19}"; fi
 
 ######################
 #   Execution part   #
